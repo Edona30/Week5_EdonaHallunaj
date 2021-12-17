f28495ed213457a0da7527d465c8372a2e5312b1
Edona30 <94362979+Edona30@users.noreply.github.com>
2021-12-17 15:43:56 +0100

    Add files via upload

---
+++ Filmjava.js
@@ -0,0 +1,23 @@
+﻿/*function openForm() {
+    let form = document.getElementsByClassName("myForm");
+    form.style.display = "block";
+}*/
+let loggedIn = false;
+
+function closeForm() {
+    let form = document.getElementById("myForm");
+    form.style.display = "none";
+}
+
+function save() {
+    let email = document.getElementByClassName("email").value;
+    localStorage.setItem('email', email);
+}
+function welcome() {
+
+
+    if (loggedIn) {
+        let email = localStorage.getByClassName("password");
+        alert("Benvenuto ");
+    }
+}
